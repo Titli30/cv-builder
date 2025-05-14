1b761bf899e4c864c59f0b09313b3b8b3dddffcd
Titli Rudra <titlirudra40@gmail.com>
2025-05-14 18:49:38 +0000

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function scrollToBuilder() {
+  document.getElementById('resume-builder').scrollIntoView({ behavior: 'smooth' });
+}
+
+function addExperience() {
+  const div = document.createElement('div');
+  div.className = 'exp-entry';
+  div.innerHTML = `
+    <input type="text" placeholder="Job Title" class="job
+::contentReference[oaicite:19]{index=19}
+ 
